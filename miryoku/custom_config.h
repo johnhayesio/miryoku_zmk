6bb0badcc2d1c4605da30f3b2d364a9be5f1d0c1
johnhayesio <johnhayesru@gmail.com>
2025-07-02 16:26:06 +0400

    Gemini hyper suggestion

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+// For Left 'G': When tapped, send 'G'. When held, send Left Hyper (LCTRL+LALT+LGUI+LSHIFT).
+#define MIRYOKU_KL_G &mt LHYPER G
+
+// For Right 'H': When tapped, send 'H'. When held, send Right Hyper (RCTRL+RALT+RGUI+RSHIFT).
+#define MIRYOKU_KR_H &mt RHYPER H
